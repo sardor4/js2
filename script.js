c6b6e30d1997534fb4a6305deaddc1bbea0db7dd
sardor4 <83544074+sardor4@users.noreply.github.com>
2021-05-03 21:57:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var second = +prompt('Введите секунды:');
+var min = second / 60;
+var hour = min / 60;
+var day = hour / 24;
+alert('Зайдите в консоль (CTRL + SIFT + I/F12)');
+console.log(second + ' секунд');
+console.log(min + ' минут');
+console.log(hour + ' часов');
+console.log(day + ' дней');
